a5836d416127adffaf7505fc302a69da30abb8cf
Dalex Davis <thedalexdavis@gmail.com>
2025-05-19 11:26:50 +0100

    cors fixed for development

--- src/main/java/com/sportscarprices/controller/CarController.java
+++ src/main/java/com/sportscarprices/controller/CarController.java
@@ -3,6 +3,7 @@ package com.sportscarprices.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,8 @@ import com.sportscarprices.entities.Car;
 import com.sportscarprices.services.CarServices;
 
 @RestController
-@RequestMapping(path = "cars")
+@RequestMapping(path = "/cars")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CarController {
 
     private final CarServices carServices;
